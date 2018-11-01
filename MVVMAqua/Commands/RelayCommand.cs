fa09b0b551d6f5ea75a7ea8692c1dba1993a70d6
IgoR-NiK <Igor_2501@mail.ru>
2018-11-01 22:51:13 +0300

    Добавлены команды

---
+++ MVVMAqua/Commands/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MvvmAqua.Commands
+{
+	public class RelayCommand : ICommand
+	{
+		readonly Action execute;
+		readonly Func<bool> canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public RelayCommand(Action execute)
+		{
+			this.execute = execute ?? throw new ArgumentNullException("execute", "Необходимо указать действие команды");
+		}
+
+		public RelayCommand(Action execute, Func<bool> canExecute) : this(execute)
+		{
+			this.canExecute = canExecute;
+		}
+
+		public void Execute(object parameter)
+		{
+			execute();
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return canExecute?.Invoke() ?? true;
+		}
+	}
+}
---
+++ MVVMAqua/Commands/RelayCommandGeneric.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MvvmAqua.Commands
+{
+	public class RelayCommand<T> : ICommand
+	{
+		readonly Action<T> execute;
+		readonly Func<T, bool> canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public RelayCommand(Action<T> execute)
+		{
+			this.execute = execute ?? throw new ArgumentNullException("execute", "Необходимо указать действие команды");
+		}
+
+		public RelayCommand(Action<T> execute, Func<T, bool> canExecute) : this(execute)
+		{
+			this.canExecute = canExecute;
+		}
+
+		public void Execute(object parameter)
+		{
+			execute((T)parameter);
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return canExecute?.Invoke((T)parameter) ?? true;
+		}
+	}
+}
