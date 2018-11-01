ba5ac82167be35add508c659bcc708b05373fa87
IgoR-NiK <Igor_2501@mail.ru>
2018-11-01 22:53:08 +0300

    Добавлены конвертеры

---
+++ MVVMAqua/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MvvmAqua.Converters
+{
+	class InvertedBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value is bool ? !(bool)value : throw new InvalidOperationException("The target must be a boolean");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value is bool ? !(bool)value : throw new InvalidOperationException("The target must be a boolean");
+		}
+	}
+}
