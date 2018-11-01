bd5dedd84b3c75435151f70931ccd2e8ebae3c97
IgoR-NiK <Igor_2501@mail.ru>
2018-11-01 22:58:58 +0300

    Добавлены перечисления

---
+++ MVVMAqua/Enums/ModalButtons.cs
@@ -0,0 +1,23 @@
+﻿namespace MvvmAqua.Enums
+{
+	/// <summary>
+	/// Количество кнопок модального окна.
+	/// </summary>
+	public enum ModalButtons
+	{
+		/// <summary>
+		/// Нет кнопок.
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// Только "Ок".
+		/// </summary>
+		Ok,
+
+		/// <summary>
+		/// "Ок" и "Отмена".
+		/// </summary>
+		OkCancel
+	}
+}
---
+++ MVVMAqua/Enums/ModalIcon.cs
@@ -0,0 +1,28 @@
+﻿namespace MvvmAqua.Enums
+{
+	/// <summary>
+	/// Тип иконки рядом с текстом в модальном окне.
+	/// </summary>
+	public enum ModalIcon
+	{
+		/// <summary>
+		/// Нет иконки.
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// Информация.
+		/// </summary>
+		Information,
+
+		/// <summary>
+		/// Ошибка.
+		/// </summary>
+		Error,
+
+		/// <summary>
+		/// Вопрос.
+		/// </summary>
+		Question
+	}
+}
