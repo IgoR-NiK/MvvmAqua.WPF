44aa5b276f8fe4467fa0b71f6e3dff39e8c7ac2a
IgoR-NiK <Igor_2501@mail.ru>
2018-11-01 23:08:05 +0300

    Добавлены представления

---
+++ MVVMAqua/Views/ModalMessageView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MvvmAqua.Views
+{
+	/// <summary>
+	/// Логика взаимодействия для ModalView.xaml
+	/// </summary>
+	partial class ModalMessageView : UserControl
+	{
+		public ModalMessageView()
+		{
+			InitializeComponent();
+		}
+	}
+}
