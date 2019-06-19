ed073180b9ae31f9c8b85b32459952ea8b097125
IgoR-NiK <Igor_2501@mail.ru>
2019-06-20 00:35:46 +0300

    + добавлена возможность создавать свой тип ошибки валидации

---
+++ MVVMAqua/Validation/IValidatable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace MVVMAqua.Validation
+{
+	public interface IValidatable<T, TResult>
+		where TResult : IValidationResult
+	{
+		bool IsValid { get; }
+
+		ObservableCollection<TResult> Errors { get; }
+
+		T Value { get; set; }
+
+
+		void AddRule(IValidationRule<T, TResult> rule);
+
+		void AddRule(Func<T, TResult> rule);
+
+		bool Validate();
+	}
+}
--- MVVMAqua/Validation/ValidationRuleWrapper.cs
+++ MVVMAqua/Validation/ValidationRuleWrapper.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MVVMAqua.Validation
 {
-	internal class ValidationRuleWrapper<T>
+	internal class ValidationRuleWrapper<T, TResult>
+		where TResult : IValidationResult
 	{
-		public Func<T, ValidationResult> ValidationRule { get; }
+		public Func<T, TResult> ValidationRule { get; }
 
 		public bool IsValidateWhenPropertyChange { get; }
 
 
-		public ValidationRuleWrapper(Func<T, ValidationResult> validationRule, bool isValidateWhenPropertyChange)
+		public ValidationRuleWrapper(Func<T, TResult> validationRule, bool isValidateWhenPropertyChange)
 		{
 			ValidationRule = validationRule;
 			IsValidateWhenPropertyChange = isValidateWhenPropertyChange;
